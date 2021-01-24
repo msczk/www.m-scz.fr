27f1e86f449fb96339faff4fe76c43787ce8dbc2
Maxime <maxime43200@gmail.com>
2021-01-24 09:49:15 +0100

    Update notification contact form

--- www/app/Http/Controllers/ContactController.php
+++ www/app/Http/Controllers/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
-use App\Notifications\contactMessage;
+use App\Notifications\contactMessageAdmin;
 use App\Models\User;
 use App\Http\Requests\ContactRequest;
 
@@ -17,9 +17,11 @@ class ContactController extends Controller
 		$subject = $request->input('subject');
 		$message = $request->input('message');
 
-		$user = new User;
-		$user->email = 'hello@m-scz.fr';
-		$user->notify(new contactMessage($email, $name, $subject, $message));
+		$admin = new User;
+		$admin->email = env('ADMIN_EMAIL');
+		
+		$admin->notify(new contactMessageAdmin($email, $name, $subject, $message));
+		
 		return redirect(route('home').'#contact')->with('success', __('Votre message a bien été envoyé !'));
 	}
 }
--- www/app/Notifications/contactMessageAdmin.php
+++ www/app/Notifications/contactMessageAdmin.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class contactMessage extends Notification
+class contactMessageAdmin extends Notification
 {
     use Queueable;
 
